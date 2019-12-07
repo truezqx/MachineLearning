cdb2f1755ba823e24ea8dcdf1884ff8bf7c159a3
your name <you@example.com>
2019-12-07 11:20:49 +0800

    creat tree

--- Ch03/trees.py
+++ Ch03/trees.py
@@ -1,5 +1,5 @@
 from math import log
-
+import operator
 
 def calcShannonEnt(dataSet):
     #计算数据集长度
@@ -73,6 +73,35 @@ def chooseBestFeatureToSplit(dataSet):
             bestInfoGain = infoGain
             bestFeature = i
     return bestFeature
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(),key = operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet,labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(dataSet):
+        return classList[0]
+    if len(dataSet[0])==1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet,bestFeat,value),subLabels)
+    return myTree
+
+
 
 if __name__=="__main__":
     dataSet,labels = createDataSet()
